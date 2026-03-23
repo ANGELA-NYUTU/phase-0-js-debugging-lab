4c452d993951f57823ec075c0f496fbd3e561143
ANGELA-NYUTU <angie.nimo254@gmail.com>
2026-03-23 04:36:17 +0300

    correct fomula

--- discountCalculator.js
+++ discountCalculator.js
@@ -11,7 +11,34 @@ function calculateDiscountedPrice(quantity, pricePerItem) {
 
     return totalPrice;
 }
+module.exports = calculateDiscountedPrice;
 
+/*Loop starts at 1 instead of 0
+Loop runs quantity - 1 times
+So one item is missing from total price calculation*/
 
+/*debugging steps include:
+debug> cont
+debug> next
+debug> repl
+> quantity
+> pricePerItem
+> i
+> totalPrice*/
 
+//Fixed code would be 
+function calculateDiscountedPrice(quantity, pricePerItem) {
+    let totalPrice = 0;
+    debugger;
+
+    for (let i = 0; i < quantity; i++) { 
+        totalPrice += pricePerItem;
+    }
+
+    if (quantity >= 10) {
+        totalPrice *= 0.9;
+    }
+
+    return totalPrice;
+}
 module.exports = calculateDiscountedPrice;
